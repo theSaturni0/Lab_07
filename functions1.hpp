63ebb90060d7dc7a029b8004a22579487351bc66
theSaturni0 <thesaturni0@gmail.com>
2025-11-14 20:06:16 +0200

    Update functions1.hpp

--- functions1.hpp
+++ functions1.hpp
@@ -5,7 +5,6 @@ namespace ProductFunctions {
 
     ProductArray* createProductArray(int size);
     void deleteProductArray(ProductArray* arr);
-    void filterProducts(ProductArray* arr, bool (*condition)(const Product*));
 
     Product* defineProduct(const char* name, float price, int quantity);
     void addProduct(ProductArray* arr, Product* prd);
