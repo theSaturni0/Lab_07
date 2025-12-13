fa76fd3c1d8c0f057075aa664ab9f8bb744900b3
theSaturni0 <thesaturni0@gmail.com>
2025-12-13 07:22:59 +0200

    pull request

--- functions1.hpp
+++ functions1.hpp
@@ -1,11 +1,10 @@
-pragma once
+#pragma once
 #include "data.hpp"
 
 namespace ProductFunctions {
 
     ProductArray* createProductArray(int size);
     void deleteProductArray(ProductArray* arr);
-    void filterProducts(ProductArray* arr, bool (*condition)(const Product*));
 
     Product* defineProduct(const char* name, float price, int quantity);
     void addProduct(ProductArray* arr, Product* prd);
@@ -17,5 +16,7 @@ namespace ProductFunctions {
     bool ammountMoreThan5(const Product* product);
     bool ammountLessThan5(const Product* product);
 
-    void printArray(ProductArray* arr);
+    void printArray(ProductArray& arr);
 }
+
+// pull request
